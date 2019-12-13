ea5533d658d2d885e61e3147d96e78fcfc2644f2
Gio Palacino <gio@palacino.net>
2019-12-13 09:15:08 -0600

    Cleans up main

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,10 @@
 import { DynamoDB } from 'aws-sdk';
 import { DynamoDbDown } from './lib/dynamoDbDown';
-import { ErrorCallback } from 'abstract-leveldown';
-
-const globalStore: { [location: string]: DynamoDbDown } = {};
 
 function factoryProvider(dynamoDb: DynamoDB) {
-  const func = function(location: string) {
-    const instance = new DynamoDbDown(dynamoDb, location);
-    globalStore[location] = instance;
-    return instance;
+  return function(location: string) {
+    return new DynamoDbDown(dynamoDb, location);
   };
-
-  func.destroy = async function(location: string, cb: ErrorCallback) {
-    const store = globalStore[location];
-    if (!store) return cb(new Error('NotFound'));
-
-    try {
-      await store.deleteTable();
-    } catch (e) {
-      if (e && e.code !== 'ResourceNotFoundException') {
-        return cb(e);
-      }
-    }
-
-    Reflect.deleteProperty(globalStore, location);
-    return cb(undefined);
-  };
-
-  return func;
 }
 
 export default factoryProvider;
